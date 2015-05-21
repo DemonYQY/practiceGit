282925df111eef19be3352104a876c442937c8b3
DemonY <avenging_demon@hotmail.com>
2015-05-21 22:26:28 +0800

    first init

---
+++ testGitSSH/testGitSSH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitSSH
+//
+//  Created by DemonY on 15/5/21.
+//  Copyright (c) 2015年 DemonY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGitSSH/testGitSSH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitSSH
+//
+//  Created by DemonY on 15/5/21.
+//  Copyright (c) 2015年 DemonY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
